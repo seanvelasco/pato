66337903933f0f218390f4d09bbc8db985d48b82
Sean Velasco <sean@sean.app>
2024-08-17 04:04:37 +0800

    Updated bot responses, fixed typo

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	ERROR        = "Pato encountered an error"
-	RESISTRECTED = ""
-	BREAK        = "Pato is taking a break. Pato will be back back in a few moments! "
+	ERROR = "Pato encountered an error."
+	EMPTY = "Pato searched far and wide but did not find anything :("
+	BREAK = "Pato is taking a break. Pato will be back soon!"
 )
